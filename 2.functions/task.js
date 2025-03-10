0fab405e5b8b35edc7e335efcdbce661aacadec6
Ambikatus <ilya.anunev@gmail.com>
2025-03-10 20:52:07 +0700

    task 2.functions done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,94 @@
 function getArrayParams(...arr) {
+	let min = arr[0];
+	let max = arr[0];
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		sum += arr[i];
+	}
+	let avg = Number((sum / arr.length).toFixed(2));
 
-  return { min: min, max: max, avg: avg };
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
 
-function summElementsWorker(...arr) {
 
+function summElementsWorker(...arr) {
+	let sum = 0
+	if (arr.length > 0) {
+		sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+	}
+
+	return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
-
+	if (arr.length > 0) {
+		let min = arr[0];
+		let max = arr[0];
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+			if (arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		return (max - min);
+	} else {
+		return 0;
+	}
 }
 
 function differenceEvenOddWorker(...arr) {
-
+	if (arr.length > 0) {
+		let sumEvenElement = 0;
+		let sumOddElement = 0;
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] % 2 === 0) {
+				sumEvenElement += arr[i];
+			} else {
+				sumOddElement += arr[i];
+			}
+		}
+		return (sumEvenElement - sumOddElement);
+	} else {
+		return 0;
+	}
 }
 
 function averageEvenElementsWorker(...arr) {
-
+	if (arr.length > 0) {
+		let sumEvenElement = 0;
+		let countEvenElement = 0;
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] % 2 === 0) {
+				sumEvenElement += arr[i];
+				countEvenElement++;
+			}
+		}
+		return sumEvenElement / countEvenElement;
+	} else {
+		return 0;
+	}
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
+	let maxWorkerResult = -Infinity;
+	for (let i = 0; i < arrOfArr.length; i++) {
+		let result = func(...arrOfArr[i]);
+		if (result > maxWorkerResult) {
+			maxWorkerResult = result;
+		}
+	}
 
+	return maxWorkerResult;
 }
