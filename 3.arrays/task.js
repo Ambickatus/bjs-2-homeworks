e51c1ed79edb0df44b6340dbaedfdd888d060b95
Ambikatus <ilya.anunev@gmail.com>
2025-04-07 14:41:42 +0700

    Task3 done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	return arr1.every((element, index) => {
+		return element === arr2[index];
+	});
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let filterUsers = users.filter(element => element.gender === gender);
+
+	if (filterUsers.length === 0)
+		return 0;
+
+	let sumOfAges = filterUsers.map(element => element.age);
+
+	let result = (sumOfAges.reduce((acc, age) => (acc + age))) / sumOfAges.length;
+
+	return result
 }
