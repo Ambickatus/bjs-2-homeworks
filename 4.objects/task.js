41a1d7754c96d2c8c3af35f836202e68e21348a7
Ambikatus <ilya.anunev@gmail.com>
2025-04-23 15:54:15 +0700

    task4 done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,38 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (!this.marks) {
+		console.log("Ошибка добавления");
+		return;
+	}
+
+	if (!!marksToAdd.length) {
+		this.marks.push(...marksToAdd);
+	}
+
+	return;
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || !this.marks.length) {
+		return 0;
+	}
+	const result = this.marks.reduce((acc, sum) => acc + sum);
+	return result / this.marks.length;
+
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
